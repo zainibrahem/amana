74f9ea8250d663e681133f0f37c9b94a42e8a6d8
zain ibra <zainibrahe@gmail.com>
2021-04-25 00:37:53 +0300

    only shop

--- CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-version: 5.0.2
-
-1. update packages
-2. fix some design issues
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-  babelrcRoots: ['.', 'packages/*'],
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = ['next/babel'];
+  const plugins = [
+    [
+      'styled-components',
+      {
+        ssr: true,
+      },
+    ],
+  ];
+
+  return {
+    presets,
+    plugins,
+  };
 };
