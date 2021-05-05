239eaa3d3d8b943a15727913bc5bf91394d4a24d
zain ibra <zainibrahe@gmail.com>
2021-05-05 15:29:34 +0300

    banner images

--- src/layouts/horizontal-category-menu/banners.tsx
+++ src/layouts/horizontal-category-menu/banners.tsx
@@ -4,6 +4,7 @@ import {
 } from './banners.style';
 import {Hover} from '../../components/banner/Hover';
 import Images from 'assets/images/03.jpg';
+import Image from 'assets/images/dummy-img-5.png';
 interface Props {
     images?:string
     title:string
@@ -19,14 +20,14 @@ return (
         </div>
         <div className="right-side">
           <div className="row1">
-            <Hover></Hover>
-            <Hover></Hover>
-            <Hover></Hover>
+            <Hover itemImage={Image}></Hover>
+            <Hover itemImage={Image}></Hover>
+            <Hover itemImage={Image}></Hover>
           </div>
           <div className="row2">
-            <Hover></Hover>
-            <Hover></Hover>
-            <Hover></Hover>
+            <Hover itemImage={Image}></Hover>
+            <Hover itemImage={Image}></Hover>
+            <Hover itemImage={Image}></Hover>
           </div>
         </div>
         <div className="anything"></div>
