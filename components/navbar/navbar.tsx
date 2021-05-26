1d05f7ac474e261fd4fa986ee9d9843bbde7f70d
zain ibrahem <zainibrahe@gmail.com>
2021-05-26 16:37:52 +0300

    last animation

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -407,7 +407,7 @@ export const NavBar = (props) => {
                             
 
                           
-                                   <div className={menu==2?"px-2 py-2 centered":"px-2 py-2  slideLeft"}>
+                                   <div className={menu==2?"px-2 py-2 slidesright":"px-2 py-2  slidesleft"}>
                                    <div className="hidden col-span-12 border-t-2 px-2 py-3  justify-between relative items-center">
                                        <img className="w-14" src="./images/med-3.jpg" alt="" />
                                        <div className="flex  flex-col justify-between items-start">
